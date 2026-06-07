0aec67b9391b39468aabf6e32cb4c699c07f7209
Tanish Singh <tanishsingh6005@gmail.com>
2026-06-07 20:37:45 +0530

    Time: 291 ms (62.3%), Space: 24.4 MB (75.41%) - LeetHub

---
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -0,0 +1,21 @@
+class Solution:
+    def createBinaryTree(self, descriptions):
+        nodes = {}
+        children = set()
+
+        for parent, child, isLeft in descriptions:
+            if parent not in nodes:
+                nodes[parent] = TreeNode(parent)
+            if child not in nodes:
+                nodes[child] = TreeNode(child)
+
+            if isLeft:
+                nodes[parent].left = nodes[child]
+            else:
+                nodes[parent].right = nodes[child]
+
+            children.add(child)
+
+        for value in nodes:
+            if value not in children:
+                return nodes[value]
